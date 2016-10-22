565c3f3285a5595bbadd0bc9884f7bb7642f3f62
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2016-10-21 20:23:43 -0500

    Yet another small update.

--- pywwwget.py
+++ pywwwget.py
@@ -414,6 +414,8 @@ def download_from_url_file_with_urllib(httpurl, httpheaders, httpcookie, buffers
  global geturls_download_sleep, tmpfileprefix, tmpfilesuffix;
  exec_time_start = time.time();
  myhash = hashlib.new("sha1");
+ if(sys.version[0]>="3"):
+  myhash = myhash.encode('utf-8');
  myhash.update(httpurl);
  myhash.update(str(buffersize));
  myhash.update(str(exec_time_start));
@@ -573,6 +575,8 @@ if(haverequests==True):
   global geturls_download_sleep, tmpfileprefix, tmpfilesuffix;
   exec_time_start = time.time();
   myhash = hashlib.new("sha1");
+  if(sys.version[0]>="3"):
+   myhash = myhash.encode('utf-8');
   myhash.update(httpurl);
   myhash.update(str(buffersize));
   myhash.update(str(exec_time_start));
@@ -743,6 +747,8 @@ if(havemechanize==True):
   global geturls_download_sleep, tmpfileprefix, tmpfilesuffix;
   exec_time_start = time.time();
   myhash = hashlib.new("sha1");
+  if(sys.version[0]>="3"):
+   myhash = myhash.encode('utf-8');
   myhash.update(httpurl);
   myhash.update(str(buffersize));
   myhash.update(str(exec_time_start));
