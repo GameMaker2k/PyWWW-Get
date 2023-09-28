b038820b8936028c706661d9829ac59203b51ed9
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-09-27 19:07:57 -0500

    Update httpserv.py

--- httpserv.py
+++ httpserv.py
@@ -19,27 +19,50 @@ pyoldver = True;
 try:
     from BaseHTTPServer import HTTPServer;
     from SimpleHTTPServer import SimpleHTTPRequestHandler;
+    from urlparse import parse_qs;
 except ImportError:
     from http.server import SimpleHTTPRequestHandler, HTTPServer;
+    from urllib.parse import parse_qs;
     pyoldver = False;
 
-
-
 class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def display_info(self):
+        # Setting headers for the response
+        self.send_response(200);
+        self.send_header('Content-type', 'text/plain');
+        self.end_headers();
+        # Displaying request method
+        response = 'Method: {}\n'.format(self.command);
+        response += 'Path: {}\n'.format(self.path);
+        # Displaying all headers
+        headers_list = ["{}: {}".format(key.title(), self.headers[key]) for key in self.headers];
+        response += '\nHeaders:\n' + '\n'.join(headers_list) + '\n';
+        # Displaying GET parameters (if any)
+        if self.command == 'GET':
+            query = self.path.split('?', 1)[-1];
+            params = parse_qs(query);
+            if params:
+                response += '\nGET Parameters:\n';
+                for key, values in params.items():
+                    response += '{}: {}\n'.format(key, ', '.join(values));
+        # Sending the response
+        self.wfile.write(response.encode('utf-8'));
     def do_GET(self):
-        # Set response status code
+        self.display_info();
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']);
+        post_data = self.rfile.read(content_length).decode('utf-8');
+        params = parse_qs(post_data);
+        # Displaying POST parameters
+        response = 'POST Parameters:\n';
+        for key, values in params.items():
+            response += '{}: {}\n'.format(key, ', '.join(values));
+        # Setting headers for the response
         self.send_response(200);
-        # Set headers
         self.send_header('Content-type', 'text/plain');
         self.end_headers();
-        # Print all headers
-        headers_list = ["{}: {}".format(key.title().title(), self.headers[key]) for key in self.headers];
-        headers_str = "\n".join(headers_list);
-        self.wfile.write(headers_str.encode('utf-8'))
-    def do_HEAD(self):
-        self.send_response(200);
-        self.send_header('Content-type', 'text/plain');
-        self.end_headers();
+        # Sending the response
+        self.wfile.write(response.encode('utf-8'));
 
 if __name__ == "__main__":
     server_address = ('', 8080);
