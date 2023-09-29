823e6ebccf4e40a171dd72848e0b88415c49a3cb
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-09-28 23:14:21 -0500

    Add files via upload

--- pywwwget.py
+++ pywwwget.py
@@ -451,6 +451,50 @@ def http_status_to_reason(code):
     }
     return reasons.get(code, 'Unknown Status Code');
 
+def ftp_status_to_reason(code):
+    reasons = {
+        110: 'Restart marker reply',
+        120: 'Service ready in nnn minutes',
+        125: 'Data connection already open; transfer starting',
+        150: 'File status okay; about to open data connection',
+        200: 'Command okay',
+        202: 'Command not implemented, superfluous at this site',
+        211: 'System status, or system help reply',
+        212: 'Directory status',
+        213: 'File status',
+        214: 'Help message',
+        215: 'NAME system type',
+        220: 'Service ready for new user',
+        221: 'Service closing control connection',
+        225: 'Data connection open; no transfer in progress',
+        226: 'Closing data connection',
+        227: 'Entering Passive Mode',
+        230: 'User logged in, proceed',
+        250: 'Requested file action okay, completed',
+        257: '"PATHNAME" created',
+        331: 'User name okay, need password',
+        332: 'Need account for login',
+        350: 'Requested file action pending further information',
+        421: 'Service not available, closing control connection',
+        425: 'Can\'t open data connection',
+        426: 'Connection closed; transfer aborted',
+        450: 'Requested file action not taken',
+        451: 'Requested action aborted. Local error in processing',
+        452: 'Requested action not taken. Insufficient storage space in system',
+        500: 'Syntax error, command unrecognized',
+        501: 'Syntax error in parameters or arguments',
+        502: 'Command not implemented',
+        503: 'Bad sequence of commands',
+        504: 'Command not implemented for that parameter',
+        530: 'Not logged in',
+        532: 'Need account for storing files',
+        550: 'Requested action not taken. File unavailable',
+        551: 'Requested action aborted. Page type unknown',
+        552: 'Requested file action aborted. Exceeded storage allocation',
+        553: 'Requested action not taken. File name not allowed'
+    };
+    return reasons.get(code, 'Unknown Status Code');
+
 def make_http_headers_from_dict_to_list(headers={'Referer': "http://google.com/", 'User-Agent': geturls_ua, 'Accept-Encoding': compression_supported, 'Accept-Language': "en-US,en;q=0.8,en-CA,en-GB;q=0.6", 'Accept-Charset': "ISO-8859-1,ISO-8859-15,utf-8;q=0.7,*;q=0.7", 'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 'Connection': "close"}):
  if isinstance(headers, dict):
   returnval = [];
