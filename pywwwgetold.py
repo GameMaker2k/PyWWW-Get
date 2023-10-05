1e22be413dd61afa5446976309fd8174575e0ff3
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-10-05 17:51:01 -0500

    Update pywwwgetold.py

--- pywwwgetold.py
+++ pywwwgetold.py
@@ -2635,7 +2635,7 @@ if(havehttpcore):
    log.info("Error With URL "+httpurl);
    return False;
   httpcodeout = geturls_text.status;
-  httpcodereason = geturls_text.reason_phrase;
+  httpcodereason = http_status_to_reason(geturls_text.status);
   httpversionout = "1.1";
   httpmethodout = httpmethod;
   httpurlout = str(httpurl);
