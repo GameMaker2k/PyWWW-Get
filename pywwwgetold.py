07e995ac7b892c03da07cd56cbe4090ea35aaf34
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-10-22 14:36:34 -0500

    Small update

--- pywwwgetold.py
+++ pywwwgetold.py
@@ -15,7 +15,7 @@
     $FileInfo: pywwwgetold.py - Last Update: 10/5/2023 Ver. 2.0.2 RC 1 - Author: cooldude2k $
 '''
 
-from __future__ import division, absolute_import, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 import re
 import os
 import sys
